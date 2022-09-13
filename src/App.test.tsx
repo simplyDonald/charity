d55f3a643f4ee76ac5f721c40c70030c7115f95c
simplyDonald <donaldabuah@gmail.com>
2022-09-13 13:24:27 -0400

    edit test file link element

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,6 +4,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/CHARITY BEGINS AT HOME/i);
   expect(linkElement).toBeInTheDocument();
 });
