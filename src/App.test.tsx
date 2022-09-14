e1f68a039025fb0f79bd112c9e8628dce62b53c9
simplyDonald <donaldabuah@gmail.com>
2022-09-14 14:24:12 -0400

    modify search element for test file

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,6 +4,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/CHARITY BEGINS AT HOME/i);
+  const linkElement = screen.getByText(/charity begins at home/i);
   expect(linkElement).toBeInTheDocument();
 });
