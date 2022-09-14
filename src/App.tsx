998bd5eda2011b5936a6f2b293cae29f563c358f
simplyDonald <donaldabuah@gmail.com>
2022-09-14 15:42:14 -0400

    add daisyui plugin and button element

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,9 @@ import React from 'react';
 function App() {
   return (
     <div className="App">
-      <div className='text-center text-7xl text-red-600'>
+      <div className="text-center text-7xl text-red-600">
         CHARITY BEGINS AT HOME
+        <button className="btn btn-primary">Button</button>
       </div>
     </div>
   );
