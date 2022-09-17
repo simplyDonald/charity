b8e19475c68fbd78b6abf01ad7d0bb7a4030842a
simplyDonald <donaldabuah@gmail.com>
2022-09-17 12:32:41 -0400

    include test file

---
+++ src/components/__test__/Navbar.test.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import Navbar from "../Navbar";
+
+test("renders daisy ui", () => {
+  render(<Navbar />);
+  const linkElement = screen.getByText(/imaginary friends/i);
+  expect(linkElement).toBeInTheDocument();
+});
