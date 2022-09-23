e8a7571d6dcfb12ecdf03c02cedb4c3487108fdf
simplyDonald <donaldabuah@gmail.com>
2022-09-22 22:40:35 -0400

    add svg divider to landing page section

--- src/App.tsx
+++ src/App.tsx
@@ -15,14 +15,28 @@ function App() {
   }, []);
   return (
     <div className=" bg-[#f4f4f9] h-screen relative ">
-
       <Navbar />
       <Navbar2 />
       <Hero />
       <div className=" h-2/3"></div>
       <Section1 />
 
-      <div className=" h-52 bg-[#d8d8d8] text-white">hello world</div>
+      <div className=" h-52 bg-[#d8d8d8] relative text-black">
+        hello world
+        <div className="custom-shape-divider-bottom-1663900606">
+          <svg
+            data-name="Layer 1"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 1200 120"
+            preserveAspectRatio="none"
+          >
+            <path
+              d="M1200 120L0 16.48 0 0 1200 0 1200 120z"
+              className="shape-fill"
+            ></path>
+          </svg>
+        </div>
+      </div>
 
       <Footer />
     </div>
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -13,7 +13,7 @@ let style: { backgroundImage: string } = {
       className="w-full bg-center bg-cover h-3/4"
       style={style}
     >
-      <div className="flex items-center justify-center w-full h-full bg-gray-900 bg-opacity-50">
+      <div className="flex items-center justify-center w-full h-full ">
         <div className="text-center">
           <h1 className="text-2xl font-semibold text-white uppercase lg:text-3xl">
             Build Your new <span className="text-blue-400 underline">Saas</span>
