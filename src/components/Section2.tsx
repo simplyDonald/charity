33930c584e7498db4d586efabd4adaffa029d17b
simplyDonald <donaldabuah@gmail.com>
2022-09-23 00:11:25 -0400

    edit text

--- src/components/Section2.tsx
+++ src/components/Section2.tsx
@@ -8,8 +8,8 @@ const Section2: FC<ISection2Props> = (props) => {
     <section className="bg-white dark:bg-gray-900">
       <div className="container px-6 py-10 mx-auto">
         <h1 className="text-3xl font-semibold text-center text-gray-800 capitalize lg:text-4xl dark:text-white">
-          explore our <br /> awesome{" "}
-          <span className="text-blue-500">Components</span>
+          explore ways <br /> you can{" "}
+          <span className="text-blue-500">help</span>
         </h1>
 
         <div className="grid grid-cols-1 gap-8 mt-8 xl:mt-12 xl:gap-16 md:grid-cols-2 xl:grid-cols-3">
